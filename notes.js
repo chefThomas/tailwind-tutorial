b8310e09be62e7b71a4aeb5c8b89e39c65eabcd5
chefthomas <thomcdixon@gmail.com>
2020-11-07 12:22:38 -0800

    inital commit

---
+++ notes.js
@@ -0,0 +1,27 @@
+/**
+ * handy dandy tailwind classes and their doc links
+ *
+ * 1
+ * CONTAINER https://tailwindcss.com/docs/container
+ * WIDTH  https://tailwindcss.com/docs/width
+ * GRID https://tailwindcss.com/docs/grid-template-columns
+ * FLEX https://tailwindcss.com/docs/flex-direction
+ * MARGIN https://tailwindcss.com/docs/margin#class-reference
+ *
+ *2
+ * BACKGROUND COLOR https://tailwindcss.com/docs/background-color#background-colors
+ * bg-color code
+ * .bg-colorcode-100-800
+ *
+ * BORDERS https://tailwindcss.com/docs/border-radius
+ * MARGINS https://tailwindcss.com/docs/margin
+ * PADDING https://tailwindcss.com/docs/padding
+ * BOX SHADOW https://tailwindcss.com/docs/box-shadow
+ *
+ * PURGE CSS
+ * Tailwind produces a lot of unused CSS which should be removed to enhance performance. Purge reduces filesize by many orders of
+ * magnitude. Ex 2mb to 10kb.
+ *
+ * For more useful resources associated with Tailwind, see https://github.com/aniftyco/awesome-tailwindcss
+ * Includes, many free templates, components and LEARNING resources
+ */
